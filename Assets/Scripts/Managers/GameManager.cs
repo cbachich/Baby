61691ed4e4e1efa1690b2c1438ba1fed9919bc80
sketchycode <jeremy@sketchyventures.com>
2018-04-22 10:54:25 -0600

    beginning dingo behavior

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+
 	[SerializeField]
 	private float startingGameTime = 240f;
 
+	[SerializeField]
+	private Dingo dingoPrefab;
+
+	[SerializeField]
+	private float dingoSpawnTime = 20f;
+
+	[SerializeField]
+	private Vector2 dingoSpawnPoint = new Vector2(6f, 0f);
+
 	private BabyGrowth[] babyGrowthTiles;
+	private float timeSinceDingoSpawn = 1000f;
 
 	public int Score { get; private set; }
 	public float TimeRemaining { get; private set; }
@@ -27,15 +38,16 @@ public class GameManager : MonoBehaviour {
 
 	private void Update() {
 		TimeRemaining -= Time.deltaTime;
+		timeSinceDingoSpawn += Time.deltaTime;
 
 		if(TimeRemaining <= 0f) {
 			TimeRemaining = 0f;
 			IsGameOver = true;
 		}
-	}
 
-	private void OnBabyGrowthCompleted(object sender, EventArgs args) {
-		Score++;
+		if(timeSinceDingoSpawn >= dingoSpawnTime && !IsGameOver) {
+			SpawnDingo();
+		}
 	}
 
 	public void RestartLevel() {
@@ -45,4 +57,19 @@ public class GameManager : MonoBehaviour {
 	public void BeginLevel() {
 		TimeRemaining = startingGameTime;
 	}
+
+	private void SpawnDingo() {
+		var dingo = GameObject.Instantiate(dingoPrefab, dingoSpawnPoint, Quaternion.identity);
+		dingo.BabyCaptured += OnDingoCapturedBaby;
+	}
+
+	#region Event Handlers
+	private void OnBabyGrowthCompleted(object sender, EventArgs args) {
+		Score++;
+	}
+
+	private void OnDingoCapturedBaby(object sender, EventArgs args) {
+		Debug.Log("dingo ate my baby!");
+	}
+	#endregion
 }
