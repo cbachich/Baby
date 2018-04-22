f458a51da9699930689ff787e0f4773d5a2a9013
Christopher Bachicha <cbachich@gmail.com>
2018-04-22 12:52:42 -0600

    Add Stork and movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,12 +7,6 @@ public class PlayerMovement : MonoBehaviour {
 	[SerializeField]
     private float speed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		var x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
         var y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
---
+++ Assets/Scripts/Stork/StorkMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FlightDirection {
+	Up,
+	Down,
+	Right,
+	Left
+}
+
+public class StorkMovement : MonoBehaviour {
+
+	[SerializeField]
+    private float speed = 1.0f;
+
+	[SerializeField]
+    private FlightDirection direction = FlightDirection.Up;
+
+	[SerializeField]
+	private Rigidbody m_Rigidbody;
+
+	public FlightDirection FlightDirection { 
+		get { return direction; } 
+	}
+
+	Vector3 flyingDirection;
+
+	// Use this for initialization
+	void Awake () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+		SetDirection();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fly();
+	}
+
+	private void Fly() {
+		m_Rigidbody.velocity = transform.up * speed;
+	}
+
+	private void SetDirection() {
+		if (direction == FlightDirection.Down) {
+			transform.Rotate (Vector3.forward * 180);
+		}
+		else if (direction == FlightDirection.Right) {
+			transform.Rotate (Vector3.forward * -90);
+		}
+		else if (direction == FlightDirection.Left) {
+			transform.Rotate (Vector3.forward * 90);
+		}
+	}
+}
