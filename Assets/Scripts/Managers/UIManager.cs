12ba0f44992d96dff936b797bf6b5f38eda1e394
sketchycode <jeremy@sketchyventures.com>
2018-04-22 19:10:34 -0600

    added baby death game over

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -11,6 +11,16 @@ public class UIManager: MonoBehaviour {
     [SerializeField]
     private GameObject gameOverPanel;
 
+    [SerializeField]
+    private Image[] babyDeathIndicators;
+
+    [SerializeField]
+    private Sprite babyFaceSilohouette;
+    [SerializeField]
+    private Sprite babyFaceDeath;
+    [SerializeField]
+    private Color babyFaceSilohouetteColor;
+
     private GameManager gameManager;
 
     private void Awake() {
@@ -22,6 +32,11 @@ public class UIManager: MonoBehaviour {
         timeRemaining.text = "Time - " + FormatTimeText((int)gameManager.TimeRemaining);
 
         gameOverPanel.SetActive(gameManager.IsGameOver);
+
+        for(int i=0; i<babyDeathIndicators.Length; i++) {
+            babyDeathIndicators[i].sprite = i >= gameManager.BabyDeathCount ? babyFaceSilohouette : babyFaceDeath;
+            babyDeathIndicators[i].color = (i >= gameManager.BabyDeathCount) ? babyFaceSilohouetteColor : Color.white;
+        }
     }
 
     private string FormatTimeText(int seconds) {
